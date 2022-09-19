13fb3752eef0629560071099c5e5be30ab9d5f8a
Golobon <golobon@ya.ru>
2022-09-19 13:06:21 +0300

    Initial commit

---
+++ src/Lecture_6/Solution_1.java
@@ -0,0 +1,14 @@
+package Lecture_6;
+
+public class Solution_1 {
+    public static void main(String[] args) {
+    }
+
+    public class Horse {
+
+    }
+
+    public class Pegasus extends Horse {
+
+    }
+}
