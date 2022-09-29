21f6ac1bb1f408420727264a19d24196519b5421
Golobon <golobon@ya.ru>
2022-09-29 23:14:12 +0300

    Commit

---
+++ src/Lecture_2/Solution_5.java
@@ -0,0 +1,23 @@
+package Lecture_2;
+
+public class Solution_5 {
+    public static void main(String[] args) {
+        print((short) 1);
+        print((Number) 1);
+        print(1);
+        print((Integer) 1);
+        print((int) 1);
+    }
+
+    public static void print(Integer i) {
+        System.out.println("Это Integer " + i);
+    }
+
+    public static void print(Object i) {
+        System.out.println("Это Object " + i);
+    }
+
+    public static void print(double i) {
+        System.out.println("Это double " + i);
+    }
+}
