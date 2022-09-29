30740fbbb2fa24ccaa572e78558934b3f4cbba78
Golobon <golobon@ya.ru>
2022-09-29 21:25:14 +0300

    Commit

---
+++ src/Lecture_2/Solution_4.java
@@ -0,0 +1,29 @@
+package Lecture_2;
+
+public class Solution_4 {
+    public interface HasWeight {
+        int getWeight();
+    }
+
+    public interface HasHeight {
+        int getHeight();
+    }
+
+    public static class Human implements HasWeight, HasHeight {
+        @Override
+        public int getWeight() {
+            return 10;
+        }
+
+        @Override
+        public int getHeight() {
+            return 20;
+        }
+    }
+
+    public static void main(String[] args) {
+        Human human = new Human();
+        System.out.println(human.getWeight());
+        System.out.println(human.getHeight());
+    }
+}
