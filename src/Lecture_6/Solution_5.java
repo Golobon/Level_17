8155572ed2c8d407711f94f35db2306c8339bdf9
Golobon <golobon@ya.ru>
2022-10-02 22:28:38 +0300

    Commit

--- src/Lecture_6/Solution_5.java
+++ src/Lecture_6/Solution_5.java
@@ -1,9 +1,16 @@
 package Lecture_6;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution_5 {
-
-        public static int catCount = 0;
-        public static String namePrefix;
-        static int maxCatCount = 50;
-
+    public static Map<Double, String> labels = new HashMap<Double, String>();
+    public static void main(String[] args) {
+        labels.put(0.1, "i");
+        labels.put(0.2, "i");
+        labels.put(0.3, "i");
+        labels.put(0.4, "i");
+        labels.put(0.5, "i");
+        System.out.println(labels);
+    }
 }
