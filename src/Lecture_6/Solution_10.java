428961ded2409752bdae83c4d0d46429f41c2ee5
Golobon <golobon@ya.ru>
2022-10-04 21:25:54 +0300

    Commit

--- src/Lecture_6/Solution_10.java
+++ src/Lecture_6/Solution_10.java
@@ -1,4 +1,30 @@
 package Lecture_6;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+/*
+Факториал
+*/
+
 public class Solution_10 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int input = Integer.parseInt(reader.readLine());
+        reader.close();
+
+        System.out.println(factorial(input));
+    }
+
+    public static String factorial(int n) {
+        BigInteger result2 = BigInteger.valueOf(1L);
+        for (int i = 1; i <= n; i++){
+            result2 = result2.multiply(BigInteger.valueOf(i));
+        }
+        return result2.toString();
+    }
 }
