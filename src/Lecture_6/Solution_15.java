b7cbab57d9badf19c07806c392d62fe877aa1ebc
Golobon <golobon@ya.ru>
2022-10-08 20:55:32 +0300

    Commit

---
+++ src/Lecture_6/Solution_15.java
@@ -0,0 +1,24 @@
+package Lecture_6;
+
+public class Solution_15 {
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(new SpecialThread());
+        thread.start();
+
+        System.out.println("*****************");
+
+        for (StackTraceElement element : Thread.currentThread().getStackTrace()) {
+            System.out.println(element);
+        }
+    }
+
+    public static class SpecialThread implements Runnable {
+
+        @Override
+        public void run() {
+            for (StackTraceElement element : Thread.currentThread().getStackTrace()) {
+                System.out.println(element);
+            }
+        }
+    }
+}
