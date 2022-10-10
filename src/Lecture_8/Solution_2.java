7b8f56159cb1ca86e5a518c1b6ea0030a87ce201
Golobon <golobon@ya.ru>
2022-10-10 21:57:03 +0300

    Commit

---
+++ src/Lecture_8/Solution_2.java
@@ -0,0 +1,20 @@
+package Lecture_8;
+
+public class Solution_2 {
+    public static volatile int numSeconds = 3;
+
+    public static void main(String[] args) throws InterruptedException {
+        RacingClock clock = new RacingClock();
+        //add your code here - добавь код тут
+    }
+
+    public static class RacingClock extends Thread {
+        public RacingClock() {
+            start();
+        }
+
+        public void run() {
+            //add your code here - добавь код тут
+        }
+    }
+}
