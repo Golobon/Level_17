43e5aa9d2ea7454d9ec687c8a1564d487dddd451
Golobon <golobon@ya.ru>
2022-10-13 23:52:59 +0300

    Commit

---
+++ src/Lecture_9_11/Solution.java
@@ -0,0 +1,83 @@
+package Lecture_9_11;
+
+import java.io.*;
+
+/*
+Последовательный вывод файлов
+*/
+
+public class Solution {
+    public static String firstFileName;
+    public static String secondFileName;
+
+    //напишите тут ваш код
+    static {
+        BufferedReader bR = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            firstFileName = bR.readLine();
+            secondFileName = bR.readLine();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException, IOException {
+        systemOutPrintln(firstFileName);
+        systemOutPrintln(secondFileName);
+    }
+
+    public static void systemOutPrintln(String fileName) throws IOException, InterruptedException {
+        ReadFileInterface f = new ReadFileThread();
+        f.setFileName(fileName);
+        f.start();
+        f.join();
+        System.out.println(f.getFileContent());
+    }
+
+    public static class ReadFileThread extends Thread implements ReadFileInterface {
+        public String fullFileName;
+
+        public StringBuilder sB = new StringBuilder().append("");
+        @Override
+        public void setFileName(String fullFileName) {
+            this.fullFileName = fullFileName;
+        }
+
+        @Override
+        public String getFileContent() {
+            if (!sB.toString().equals("")) return sB.toString().substring(0, sB.length() - 1);
+            else return "";
+        }
+
+        @Override
+        public void start() {
+            run();
+        }
+
+        @Override
+        public void run() {
+            try {
+                BufferedReader bR2 = new BufferedReader(new FileReader(fullFileName));
+                while (bR2.ready()) {
+                    bR2.lines().forEach((o) -> sB.append(o).append(" "));
+                } Thread.currentThread().interrupt();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public interface ReadFileInterface {
+
+        void setFileName(String fullFileName);
+
+        String getFileContent() throws IOException;
+
+        void join() throws InterruptedException;
+
+        void start();
+    }
+
+    //напишите тут ваш код
+}
